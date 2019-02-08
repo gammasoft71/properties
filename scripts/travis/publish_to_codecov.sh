8c1370d56784c484f9fdf51a2762b8f80868d7a7
Gammasoft71 <gammasoft71@gmail.com>
2019-02-08 19:22:06 +0100

    change codecov token

--- scripts/travis/publish_to_codecov.sh
+++ scripts/travis/publish_to_codecov.sh
@@ -3,5 +3,5 @@
 set -ev
 
 # publish to codecov
-export CODECOV_TOKEN="dc574c52-8843-42a2-b2fe-5d58506b41d0"
+export CODECOV_TOKEN="d2fd298e-9f95-481a-a1f6-b852a0971c47"
 bash <(curl -s https://codecov.io/bash)
